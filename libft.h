66ea585d8f4b40a0d8d8b87b8e2f9c09d223f2c0
Juan <juanmiguelez2005@gmail.com>
2025-10-06 16:38:21 +0100

    tst

--- libft.h
+++ libft.h
@@ -51,6 +51,16 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstsize(t_list *lst);
+void	ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
 size_t	ft_strnlen(const char *str, size_t maxlen);
 size_t	ft_strlen_arraybi(const char **str);
 size_t	ft_strlen_intarray(const int *str);
